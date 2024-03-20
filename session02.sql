914e5d8dd2740f0bbfa2feb7bed7dd72d7630ea1
ngoc0900 <hongngoc4532@gmail.com>
2024-03-21 08:33:36 +0900

    add

---
+++ session02.sql
@@ -0,0 +1,10 @@
+create database btvn_bai1;
+-- >	Từ mô hình thự thể quan hệ ERD trên chuyển sang mô hình dữ liệu quan hệ
+-- Phiếu xuất(SỐ PX, ngày xuất)
+-- Phiếu xuất chi tiết(SỐ PX, MÃ VT, đơn giá xuất, số lượng xuất)
+-- Vật tư(MÃ VT, TÊN VT)
+-- Phiếu nhập(SỐ PN, ngày nhập)
+-- Phiếu nhập chi tiết(SỐ PN, MÃ VT, đơn giá nhập, số lượng nhập)
+-- Chi tiết đơn đặt hàng(MÃ VT, SỐ ĐH)
+-- Đơn đặt hàng( SỐ ĐH, ngày đặt hàng , MÃ NCC)
+-- Nhà cung cấp (MÃ NCC, tên ncc, địa chỉ, số điện thoại)
